49ac191321565ab8e35d4264a81e9c676fae5c40
alexshiao99 <alexshiao99@gmail.com>
2022-02-24 16:23:10 -0800

    Finished up all tasks and passed all tests

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,23 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.stackheight = 0;
+  }
+  push(value) {
+    this.stackheight++;
+    this['storage'][this.stackheight] = value;
+  }
+  pop() {
+    if (this.size() > 0 ) {
+      let top = this['storage'][this.stackheight];
+      delete this['storage'][this.stackheight];
+      this.stackheight--;
+      return top;
+    }
+  }
+  size() {
+    return this.stackheight;
   }
 
 }
